49acdfe2b5f776d5c410fd8befa1957b190b1c89
David Ortiz <judaortizchico@gmail.com>
2026-03-10 03:28:25 -0500

    initial commit

---
+++ src/test/java/com/mapuMarket/MapuMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapuMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapuMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
